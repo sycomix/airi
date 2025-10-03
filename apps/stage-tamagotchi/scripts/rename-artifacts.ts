a866c5a544979becff2a63759ece31afe7e28a6a
Neko Ayaka <neko@ayaka.moe>
2025-10-04 01:45:10 +0800

    chore(ci): fix artifacts

---
+++ apps/stage-tamagotchi/scripts/rename-artifacts.ts
@@ -0,0 +1,111 @@
+import process from 'node:process'
+
+import { mkdirSync, readdirSync, renameSync } from 'node:fs'
+import { join } from 'node:path'
+
+import { cac } from 'cac'
+
+import packageJSON from '../package.json' assert { type: 'json' }
+
+import { getElectronBuilderConfig, getFilename, getVersion } from './utils'
+
+async function main() {
+  const cli = cac('rename-artifact')
+    .option(
+      '--release',
+      'Rename with version from package.json',
+      { default: false },
+    )
+    .option(
+      '--auto-tag',
+      'Automatically tag the release with the latest git ref',
+      { default: false },
+    )
+    .option(
+      '--tag <tag>',
+      'Tag to use for the release',
+      { default: '', type: [String] },
+    )
+
+  const args = cli.parse()
+
+  let version = packageJSON.version
+  const electronBuilderConfig = await getElectronBuilderConfig()
+  const target = args.args[0]
+  const productName = electronBuilderConfig.productName
+  const dirname = import.meta.dirname
+
+  const beforeVersion = version
+  const beforeProductName = productName
+
+  const argOptions = args.options as {
+    release: boolean
+    autoTag: boolean
+    tag: string[]
+  }
+
+  version = await getVersion(argOptions)
+
+  console.log('target:', target)
+  console.log('dirname', dirname)
+  console.log('version from:', beforeVersion, 'to:', version)
+  console.log('product name from:', beforeProductName, 'to:', productName)
+
+  if (!target) {
+    throw new Error('<Target> is required')
+  }
+
+  const srcPrefix = join(dirname, '..', 'dist')
+  console.log('source directory:', srcPrefix)
+  const bundlePrefix = join(dirname, '..', 'bundle')
+  console.log('bundle directory:', bundlePrefix)
+
+  console.log('renaming directory from:', srcPrefix)
+  console.log('renaming directory to:', bundlePrefix)
+  console.log(readdirSync(srcPrefix))
+
+  mkdirSync(bundlePrefix, { recursive: true })
+
+  let renameFrom = ''
+  let renameTo = ''
+  const filename = await getFilename(target, argOptions)
+  console.log(filename, 'is the target filename')
+
+  switch (target) {
+    case 'x86_64-pc-windows-msvc':
+      renameFrom = join(srcPrefix, `${beforeProductName}-${beforeVersion}-windows-x64-setup.exe`)
+      renameTo = join(bundlePrefix, filename)
+      break
+    case 'x86_64-unknown-linux-gnu':
+      renameFrom = join(srcPrefix, 'appimage', `${beforeProductName}-${beforeVersion}-linux-amd64.AppImage`)
+      renameTo = join(bundlePrefix, filename)
+      break
+    case 'aarch64-unknown-linux-gnu':
+      renameFrom = join(srcPrefix, 'appimage', `${beforeProductName}-${beforeVersion}-linux-arm64.AppImage`)
+      renameTo = join(bundlePrefix, filename)
+      break
+    case 'aarch64-apple-darwin':
+      renameFrom = join(srcPrefix, `${beforeProductName}-${beforeVersion}-darwin-arm64.dmg`)
+      renameTo = join(bundlePrefix, filename)
+      break
+    case 'x86_64-apple-darwin':
+      renameFrom = join(srcPrefix, `${beforeProductName}-${beforeVersion}-darwin-x64.dmg`)
+      renameTo = join(bundlePrefix, filename)
+      break
+    default:
+      console.error('Target is not supported')
+      process.exit(1)
+  }
+
+  console.log('renaming, from:', renameFrom, 'to:', renameTo)
+  renameSync(renameFrom, renameTo)
+}
+
+main()
+  .then(() => {
+    console.log('Renaming completed successfully.')
+  })
+  .catch((error) => {
+    console.error('Error during renaming:', error)
+    process.exit(1)
+  })
---
+++ apps/stage-tamagotchi/scripts/utils.ts
@@ -0,0 +1,128 @@
+import type { Configuration } from 'electron-builder'
+
+import process from 'node:process'
+
+import { readFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+
+import { execa } from 'execa'
+
+import * as yaml from 'yaml'
+
+import packageJSON from '../package.json' assert { type: 'json' }
+
+export async function getVersion(options: { release: boolean, autoTag: boolean, tag: string[] }) {
+  if (!options.release || !options.tag) {
+    // Otherwise, fetch from the latest git ref
+    const res = await execa('git', ['log', '-1', '--pretty=format:"%H"'])
+    const date = new Date().toISOString().split('T')[0].replace(/-/g, '')
+    return `nightly-${date}-${String(res.stdout.replace(/"/g, '')).trim().substring(0, 7)}`
+  }
+
+  // If --release is specified, use the version from package.json
+  let version = packageJSON.version
+
+  // If --tag is specified, use the provided tag
+  if (options.tag[0] !== 'true') {
+    version = String(options.tag[0]).replace(/^v/, '').trim()
+  }
+  // Otherwise, even for --tag option (true / enabled), ignore the input
+  else {
+    version = ''
+  }
+
+  if (version) {
+    return version
+  }
+
+  // If no version is provided and --auto-tag is not specified, throw an error
+  if (!options.autoTag) {
+    throw new Error('Tag cannot be empty when --release is specified')
+  }
+
+  // Now, only auto-tag & release && non-specific tag is the only possibility,
+  // fetch the latest git ref
+  try {
+    const res = await execa('git', ['describe', '--tags', '--abbrev=0'])
+    return String(res.stdout).replace(/^v/, '').trim()
+  }
+  catch {
+    // If no tags exist, fall back to package.json version
+    console.warn('No git tags found, falling back to package.json version')
+    return packageJSON.version
+  }
+}
+
+export async function getElectronBuilderConfig() {
+  return yaml.parse(await readFile(resolve(import.meta.dirname, '..', 'electron-builder.yml'), 'utf-8')) as Configuration
+}
+
+export async function getFilename(target: string, options: { release: boolean, autoTag: boolean, tag: string[] }) {
+  const electronBuilder = await getElectronBuilderConfig()
+  const version = await getVersion(options)
+
+  if (!target) {
+    throw new Error('<Target> is required')
+  }
+
+  switch (target) {
+    case 'x86_64-pc-windows-msvc':
+
+      return electronBuilder.nsis?.artifactName
+        // eslint-disable-next-line no-template-curly-in-string
+        ?.replace('${productName}', electronBuilder.productName!)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${version}', version)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${arch}', 'x64')
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${ext}', 'exe') ?? `${electronBuilder.productName}-${version}-windows-x64-setup.exe`
+      break
+    case 'x86_64-unknown-linux-gnu':
+      return electronBuilder.linux?.artifactName
+        // eslint-disable-next-line no-template-curly-in-string
+        ?.replace('${productName}', electronBuilder.productName!)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${version}', version)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${arch}', 'x64')
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${ext}', 'AppImage') ?? `${electronBuilder.productName}-${version}-linux-amd64.AppImage`
+      break
+    case 'aarch64-unknown-linux-gnu':
+      return electronBuilder.linux?.artifactName
+        // eslint-disable-next-line no-template-curly-in-string
+        ?.replace('${productName}', electronBuilder.productName!)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${version}', version)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${arch}', 'arm64')
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${ext}', 'AppImage') ?? `${electronBuilder.productName}-${version}-linux-arm64.AppImage`
+      break
+    case 'aarch64-apple-darwin':
+      return electronBuilder.dmg?.artifactName
+        // eslint-disable-next-line no-template-curly-in-string
+        ?.replace('${productName}', electronBuilder.productName!)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${version}', version)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${arch}', 'arm64')
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${ext}', 'dmg') ?? `${electronBuilder.productName}-${version}-darwin-arm64.dmg`
+      break
+    case 'x86_64-apple-darwin':
+      return electronBuilder.dmg?.artifactName
+        // eslint-disable-next-line no-template-curly-in-string
+        ?.replace('${productName}', electronBuilder.productName!)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${version}', version)
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${arch}', 'x64')
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('${ext}', 'dmg') ?? `${electronBuilder.productName}-${version}-darwin-x64.dmg`
+    default:
+      console.error('Target is not supported')
+      process.exit(1)
+  }
+}
