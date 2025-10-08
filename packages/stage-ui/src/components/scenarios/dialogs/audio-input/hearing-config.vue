c884b758fa9621f40b7c6c110ba0a40cc852d1f9
Neko Ayaka <neko@ayaka.moe>
2025-10-08 17:14:08 +0800

    feat(stage-tamagotchi): hearing config dialog

---
+++ packages/stage-ui/src/components/scenarios/dialogs/audio-input/hearing-config.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useToggle } from '@vueuse/core'
+import { storeToRefs } from 'pinia'
+
+import Button from '../../../misc/Button.vue'
+
+import { useSettingsAudioDevice } from '../../../../stores/settings'
+
+const settingsAudioDeviceStore = useSettingsAudioDevice()
+const { enabled } = storeToRefs(settingsAudioDeviceStore)
+const toggleAudioDevice = useToggle(enabled)
+</script>
+
+<template>
+  <div>
+    <div>
+      <Button @click="() => toggleAudioDevice()">
+        <span>{{ enabled ? 'Disable Microphone' : 'Enable Microphone' }}</span>
+      </Button>
+    </div>
+  </div>
+</template>
--- packages/stage-ui/src/components/scenarios/dialogs/index.ts
+++ packages/stage-ui/src/components/scenarios/dialogs/index.ts
@@ -1,2 +1,3 @@
 export * from './about'
+export * from './audio-input'
 export * from './onboarding'
