4534664cbaf159bdab959c62b7b1d5a0ffb80342
Neko Ayaka <neko@ayaka.moe>
2025-10-12 03:31:04 +0800

    fix(stage-tamagotchi): not able to route to /settings for hash router

--- apps/stage-tamagotchi/src/main/libs/electron/location.ts
+++ apps/stage-tamagotchi/src/main/libs/electron/location.ts
@@ -1,4 +1,4 @@
-import type { BrowserWindow } from 'electron'
+import type { BrowserWindow, LoadFileOptions, LoadURLOptions } from 'electron'
 
 import { join } from 'node:path'
 import { env } from 'node:process'
@@ -24,12 +24,12 @@ export function baseUrl(parentOfIndexHtml: string) {
   }
 }
 
-export async function load(window: BrowserWindow, url: string | { url: string } | { file: string }) {
+export async function load(window: BrowserWindow, url: string | { url: string, options?: LoadURLOptions } | { file: string, options?: LoadFileOptions }) {
   if (typeof url === 'object' && 'url' in url) {
-    return await window.loadURL(url.url)
+    return await window.loadURL(url.url, url.options)
   }
   if (typeof url === 'object' && 'file' in url) {
-    return await window.loadFile(url.file)
+    return await window.loadFile(url.file, url.options)
   }
 
   return await window.loadURL(url)
@@ -37,10 +37,10 @@ export async function load(window: BrowserWindow, url: string | { url: string } 
 
 export function withHashRoute(baseUrl: string | { url: string } | { file: string }, hashRoute: string) {
   if (typeof baseUrl === 'object' && 'url' in baseUrl) {
-    return { url: `${baseUrl.url}/#${hashRoute}` }
+    return { url: `${baseUrl.url}/#${hashRoute}` } satisfies { url: string, options?: LoadURLOptions }
   }
   if (typeof baseUrl === 'object' && 'file' in baseUrl) {
-    return { file: `${baseUrl.file}#${hashRoute}` }
+    return { file: `${baseUrl.file}`, options: { hash: hashRoute } } satisfies { file: string, options?: LoadFileOptions }
   }
 
   return `${baseUrl}/#${hashRoute}`
